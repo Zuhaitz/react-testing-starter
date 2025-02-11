12a71915c28f6bc568100ad994fe105187fbc671
Zuhaitz Martínez <zuhaitzmartinezjimenez@gmail.com>
2025-02-11 13:13:15 +0100

    testing rendering

---
+++ tests/components/Greet.test.tsx
@@ -0,0 +1,23 @@
+import { it, expect, describe } from "vitest";
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom/vitest";
+
+import Greet from "../../src/components/Greet";
+
+describe("GreetComponent", () => {
+  it("should render Hello with the name when name provided", () => {
+    render(<Greet name="Zuhaitz" />);
+
+    const heading = screen.getByRole("heading");
+    expect(heading).toBeInTheDocument();
+    expect(heading).toHaveTextContent(/zuhaitz/i);
+  });
+
+  it("should render login button when name is not provided", () => {
+    render(<Greet />);
+
+    const button = screen.getByRole("button");
+    expect(button).toBeInTheDocument();
+    expect(button).toHaveTextContent(/login/i);
+  });
+});
