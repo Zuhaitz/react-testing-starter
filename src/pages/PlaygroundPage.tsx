26abf443a1c6926cf7b5f74b8fbdcc20162a1b1a
Zuhaitz Martínez <zuhaitzmartinezjimenez@gmail.com>
2025-02-14 12:56:50 +0100

    expandable text component testing done

--- src/pages/PlaygroundPage.tsx
+++ src/pages/PlaygroundPage.tsx
@@ -1,8 +1,11 @@
+import ExpandableText from "../components/ExpandableText";
 import Onboarding from "../components/Onboarding";
 import TermsAndConditions from "../components/TermsAndConditions";
 
 const PlaygroundPage = () => {
-  return <TermsAndConditions />;
+  return (
+    <ExpandableText text="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquam doloribus vitae rerum omnis consequatur, corporis quam est ea, nostrum sunt ab ipsum nam quos at harum quas reiciendis deserunt! Mollitia itaque eum odio qui, laboriosam officiis hic voluptas omnis alias distinctio fuga? Eaque eligendi fugiat sequi perferendis quod delectus quas temporibus consequuntur, nostrum reiciendis accusamus illo dignissimos possimus pariatur iste totam modi tenetur a cum rerum vero enim neque? Blanditiis, odio culpa quam, praesentium reiciendis consectetur veniam tempore nesciunt quaerat aut nihil quisquam commodi mollitia odit animi accusamus dolor doloremque, deleniti dicta voluptate deserunt! Dolor eum voluptates exercitationem nam odit." />
+  );
 };
 
 export default PlaygroundPage;
---
+++ tests/components/ExpandableText.test.tsx
@@ -0,0 +1,54 @@
+import { queryByRole, render, screen } from "@testing-library/react";
+import ExpandableText from "../../src/components/ExpandableText";
+import userEvent from "@testing-library/user-event";
+
+describe("ExpandableTextComponent", () => {
+  const limit = 255;
+  const text = "Short text";
+  const longText = "a".repeat(limit + 1);
+  const truncated = longText.substring(0, limit) + "...";
+
+  it(`should render full text if shorter than ${limit} characters`, () => {
+    render(<ExpandableText text={text} />);
+
+    expect(screen.getByText(text)).toBeInTheDocument();
+  });
+
+  it(`should truncate text if longer than ${limit} characters`, () => {
+    render(<ExpandableText text={longText} />);
+
+    expect(screen.getByText(truncated)).toBeInTheDocument();
+
+    const btn = screen.getByRole("button");
+    expect(btn).toBeInTheDocument();
+    expect(btn).toHaveTextContent(/more/i);
+  });
+
+  it("should render full text when Show More button is clicked", async () => {
+    render(<ExpandableText text={longText} />);
+
+    const btn = screen.getByRole("button");
+    const user = userEvent.setup();
+    await user.click(btn);
+
+    expect(screen.getByText(longText)).toBeInTheDocument();
+    expect(btn).toBeInTheDocument();
+    expect(btn).toHaveTextContent(/less/i);
+  });
+
+  it("should truncate text when Show Less button is clicked", async () => {
+    render(<ExpandableText text={longText} />);
+
+    const showMoreBtn = screen.getByRole("button", { name: /more/i });
+    const user = userEvent.setup();
+    await user.click(showMoreBtn);
+
+    // Collapse
+    const showLessBtn = screen.getByRole("button", { name: /less/i });
+    await user.click(showLessBtn);
+
+    expect(screen.getByText(truncated)).toBeInTheDocument();
+    expect(showMoreBtn).toBeInTheDocument();
+    expect(showMoreBtn).toHaveTextContent(/more/i);
+  });
+});
