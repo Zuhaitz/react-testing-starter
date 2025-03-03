f7d16c2640d3bb05fec41e602139315a5bfeb3a3
Zuhaitz Martínez <zuhaitzmartinezjimenez@gmail.com>
2025-03-03 14:36:45 +0100

    auth testing done

--- tests/components/AuthStatus.test.tsx
+++ tests/components/AuthStatus.test.tsx
@@ -1,46 +1,82 @@
-import { render, screen, waitFor } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
+import * as auth0 from "@auth0/auth0-react";
 
 import AuthStatus from "../../src/components/AuthStatus";
 
-import * as auth0 from "@auth0/auth0-react";
-
 vi.mock("@auth0/auth0-react");
+
+type ReturnValues = {
+  user?: {
+    name: string;
+    email: string;
+  };
+  isAuthenticated: boolean;
+  isLoading: boolean;
+  loginWithRedirect?: any;
+  logout?: any;
+};
 
 // https://stackoverflow.com/questions/72732768/vue-how-to-mock-auth0-for-testing-with-vitest
 describe("AuthStatusComponent", () => {
-  const renderWithProvider = (comp: JSX.Element) => {
-    return render(comp);
+  const renderComponent = (returnValues: ReturnValues) => {
+    (auth0 as any).useAuth0 = vi.fn().mockReturnValue(returnValues);
+    render(<AuthStatus />);
   };
 
-  it("should", async () => {
+  it("should render login button and redirect when clicked", async () => {
     const loginWithRedirect = vi.fn();
-    (auth0 as any).useAuth0 = vi.fn().mockReturnValue({
-      // user: { name: "John Doe", email: "john@example.com" },
+    renderComponent({
       isAuthenticated: false,
       isLoading: false,
       loginWithRedirect,
     });
 
-    renderWithProvider(<AuthStatus />);
-
-    screen.debug();
-
-    const login = await waitFor(() => {
-      return screen.getByRole("button");
-    });
+    const login = screen.getByRole("button");
 
     expect(login).toHaveTextContent(/in/i);
-
     const user = userEvent.setup();
     await user.click(login);
 
     expect(auth0.useAuth0).toHaveBeenCalled();
     expect(loginWithRedirect).toHaveBeenCalled();
+  });
 
-    // await waitFor(() => {
-    //   return screen.getByText(/welcome/i);
-    // });
+  it("should display something when loading", () => {
+    renderComponent({
+      isAuthenticated: false,
+      isLoading: true,
+    });
+
+    screen.getByText(/loading/i);
+  });
+
+  it("should render log out button and user name when logged in", () => {
+    renderComponent({
+      user: { name: "John Doe", email: "john@example.com" },
+      isAuthenticated: true,
+      isLoading: false,
+    });
+
+    expect(screen.getByRole("button")).toHaveTextContent(/out/i);
+    screen.getByText("John Doe");
+  });
+
+  it("should", () => {
+    const logout = vi.fn();
+    renderComponent({
+      user: { name: "John Doe", email: "john@example.com" },
+      isAuthenticated: true,
+      isLoading: false,
+      logout,
+    });
+
+    const logoutButton = screen.getByRole("button");
+    const user = userEvent.setup();
+    user.click(logoutButton);
+
+    expect(auth0.useAuth0).toHaveBeenCalled();
+    // expect(logout).toHaveBeenCalled();
   });
 
   afterEach(() => vi.clearAllMocks());
