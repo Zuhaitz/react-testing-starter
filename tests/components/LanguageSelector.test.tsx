3a53d69754362f7ffb7081b8cf86293a2f969969
Zuhaitz Martínez <zuhaitzmartinezjimenez@gmail.com>
2025-03-07 15:19:15 +0100

    language changer almost done

--- tests/components/LanguageSelector.test.tsx
+++ tests/components/LanguageSelector.test.tsx
@@ -3,6 +3,7 @@ import { MemoryRouter } from "react-router-dom";
 
 import LanguageSelector from "../../src/components/LanguageSelector";
 import Providers from "../../src/providers";
+import userEvent from "@testing-library/user-event";
 
 // Ref: https://stackoverflow.com/questions/68679993/referenceerror-resizeobserver-is-not-defined
 // Mock the ResizeObserver
@@ -15,6 +16,10 @@ const ResizeObserverMock = vi.fn(() => ({
 // Stub the global ResizeObserver
 vi.stubGlobal("ResizeObserver", ResizeObserverMock);
 
+// Ref: https://github.com/testing-library/user-event/discussions/1087
+window.HTMLElement.prototype.hasPointerCapture = vi.fn();
+window.HTMLElement.prototype.scrollIntoView = vi.fn();
+
 describe("LanguageSelector", () => {
   beforeEach(() => {
     render(<LanguageSelector />, {
@@ -26,7 +31,20 @@ describe("LanguageSelector", () => {
     });
   });
 
-  it("should", () => {
+  it("should", async () => {
+    // screen.debug();
+    const user = userEvent.setup();
+
+    const button = screen.getByRole("combobox");
+    await user.click(button);
+
     screen.debug();
+
+    const options = screen.getAllByRole("option");
+    console.log(options);
+    options.forEach((op) => {
+      console.log(op.textContent);
+    });
+    // const menuItems = screen.getAllByRole("menuitem");
   });
 });
