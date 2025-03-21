d2b5877c6c69aabc215806bc6ede51ca61741117
Zuhaitz Martínez <zuhaitzmartinezjimenez@gmail.com>
2025-03-21 11:34:42 +0100

    removed unused import

--- tests/components/Navbar.test.tsx
+++ tests/components/Navbar.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen, waitFor } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { MemoryRouter, Route, Routes } from "react-router-dom";
 
 import Providers from "../../src/providers";
