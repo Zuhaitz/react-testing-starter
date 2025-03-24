d9283555363baa5ad6db0bc8792406dcb5032f84
Zuhaitz Martínez <zuhaitzmartinezjimenez@gmail.com>
2025-03-24 14:21:38 +0100

    useCategories hook tested

---
+++ tests/hooks/useCategories.test.tsx
@@ -0,0 +1,24 @@
+import { renderHook, waitFor } from "@testing-library/react";
+
+import ReactQueryProvider from "../../src/providers/ReactQueryProvider";
+import useCategories from "../../src/hooks/useCategories";
+
+describe("useCategories", () => {
+  const wrapper = ({ children }: any) => (
+    <ReactQueryProvider>{children}</ReactQueryProvider>
+  );
+
+  it("should", async () => {
+    const { result } = renderHook(() => useCategories(), { wrapper });
+    // Wait for data to be fetched
+    await waitFor(() => expect(result.current.isLoading).toBe(false), {
+      onTimeout: (error: Error) => {
+        console.error(result.current.error);
+        return error;
+      },
+    });
+
+    expect(result.current.data).not.undefined;
+    expect(result.current.data?.length).greaterThan(0);
+  });
+});
