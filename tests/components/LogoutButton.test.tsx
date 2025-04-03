0df9256d4f5ff9eb37628fdb36c23a76d4334547
Zuhaitz Martínez <zuhaitzmartinezjimenez@gmail.com>
2025-04-03 14:57:48 +0200

    LogoutButton component test done

---
+++ tests/components/LogoutButton.test.tsx
@@ -0,0 +1,24 @@
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+
+import LogoutButton from "../../src/components/LogoutButton";
+
+import * as auth0 from "@auth0/auth0-react";
+vi.mock("@auth0/auth0-react");
+
+describe("LogoutButton", () => {
+  const logout = vi.fn();
+  (auth0 as any).useAuth0 = vi.fn().mockReturnValue({
+    isAuthenticated: false,
+    isLoading: false,
+    logout,
+  });
+
+  it("should", async () => {
+    render(<LogoutButton />);
+
+    const user = userEvent.setup();
+    await user.click(screen.getByRole("button"));
+    expect(logout).toBeCalled();
+  });
+});
